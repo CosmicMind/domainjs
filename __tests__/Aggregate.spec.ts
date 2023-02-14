684ccb870886a774dd9b10feab21586014d0f8e7
Daniel Jonathan <daniel@cosmicverse.org>
2023-02-14 12:49:11 -0600

    feat(CM-199): cleanup stringify and parse

--- __tests__/Aggregate.spec.ts
+++ __tests__/Aggregate.spec.ts
@@ -1,4 +1,34 @@
-/* Copyright (C) 2022, CosmicMind, Inc. <http://cosmicmind.com>. All rights reserved. */
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Daniel Jonathan <daniel at cosmicmind dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES LOSS OF USE, DATA, OR PROFITS OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import {
   it,
@@ -49,7 +79,7 @@ type UserRegisterEvent = Event & {
 const createUserAggregateRegisterEvent = defineEvent<UserRegisterEvent>({
   attributes: {
     entity: {
-      validate: (entity: User): boolean => guard(entity),
+      validate: (entity: User): boolean => guard<User>(entity),
     },
   },
 })
@@ -107,11 +137,11 @@ describe('Aggregate', () => {
 
     const createAggregate = defineAggregate(UserAggregate, {
       trace(entity: User) {
-        expect(guard(entity)).toBeTruthy()
+        expect(guard<User>(entity)).toBeTruthy()
       },
 
       createdAt(entity: User) {
-        expect(guard(entity)).toBeTruthy()
+        expect(guard<User>(entity)).toBeTruthy()
       },
 
       attributes: {
@@ -125,7 +155,7 @@ describe('Aggregate', () => {
         createdAt: {
           validate(value: Date) {
             expect(value).toBe(createdAt)
-            return true
+            return guard<Date>(value)
           },
         },
 
--- __tests__/Event.spec.ts
+++ __tests__/Event.spec.ts
@@ -1,4 +1,34 @@
-/* Copyright (C) 2022, CosmicMind, Inc. <http://cosmicmind.com>. All rights reserved. */
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Daniel Jonathan <daniel at cosmicmind dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES LOSS OF USE, DATA, OR PROFITS OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import {
   it,
@@ -27,10 +57,10 @@ const createUserEvent = defineEvent<Event>({
       validate: (value: string): boolean => 2 < value.length,
     },
     createdAt: {
-      validate: (value: Date): boolean => guard(value),
+      validate: (value: Date): boolean => guard<Date>(value),
     },
     entity: {
-      validate: (value: User): boolean => guard(value),
+      validate: (value: User): boolean => guard<User>(value),
     },
   },
 })
@@ -71,11 +101,11 @@ describe('Event', () => {
 
     const createEvent = defineEvent<Event>({
       trace(event: Event) {
-        expect(guard(event)).toBeTruthy()
+        expect(guard<Event>(event)).toBeTruthy()
       },
 
       createdAt(event: Event) {
-        expect(guard(event)).toBeTruthy()
+        expect(guard<Event>(event)).toBeTruthy()
       },
 
       attributes: {
@@ -96,14 +126,14 @@ describe('Event', () => {
         createdAt: {
           validate: (value: Date): boolean => {
             expect(value).toBe(createdAt)
-            return guard(value)
+            return guard<Date>(value)
           },
         },
 
         entity: {
           validate: (value: User): boolean => {
-            expect(guard(value)).toBeTruthy()
-            return guard(value)
+            expect(guard<User>(value)).toBeTruthy()
+            return guard<User>(value)
           },
         },
       },
