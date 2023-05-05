4a290b0e1c6dcda9af65394a25f8383f3fece700
Daniel Jonathan <daniel@cosmicverse.org>
2023-05-05 01:50:08 -0600

    feat(CM-246): updated adaptive layout system

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -48,7 +48,7 @@ const srcDir = 'src'
 const entry = `${srcDir}/index.ts`
 const fileName = 'lib-[format]'
 const formats: LibraryFormats[] = [ 'es', 'cjs' ]
-const emptyOutDir = false
+const emptyOutDir = true
 const minify = 'development' !== process.env.NODE_ENV
 
 const alias = {
